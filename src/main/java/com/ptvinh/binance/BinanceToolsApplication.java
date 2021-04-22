1a90a8e9aeb086b8bbc4bc58c010178557e247dc
Vinh Pham <ptvinhtech13@gmail.com>
2021-04-22 20:41:28 +0700

    Init code

---
+++ src/main/java/com/ptvinh/binance/BinanceToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ptvinh.binance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceToolsApplication.class, args);
+	}
+
+}
